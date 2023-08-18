2f20be1795bb86619294fff4d93ecce7e26d4f6d
Joshua Stout <79609659+JoshStout@users.noreply.github.com>
2023-08-17 23:11:43 -0500

    Update FSRSensorArray.ino

--- Arduino/FSRSensorArray/FSRSensorArray.ino
+++ Arduino/FSRSensorArray/FSRSensorArray.ino
@@ -1,16 +1,7 @@
-/*
-  Written for the Arduino to display the number of items resting on Force Sensitive Resistor array.
-  Each item is placed directly over a single FSR sensor. The analog pins of the Arduino monitor the junction 
-  between the sensor in series with a 10K resistor. A 5V source is supplied to the FSR and the 10K resistor 
-  is connected to ground. When the pin measures 4500mV drop across the 10K resistor, the MCU counts as 
-  an item present on the sensor. 
-*/
-
 #include<FSRMonitor.h>
 
 const int numSensors = 4; // number of sensor to monitor
-const int fsrThreshold = 4500; // if the sensor reads greather than 4950mV, then the object is present
-const int outputPin = 13; // pin monitored by the next stage
+const int fsrThreshold = 4500; // the object is present if the reading is greather than 4500mV
 
 FSRMonitor *sensorsArray[numSensors];
 
